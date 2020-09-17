2a793c25c7788929dcb3dbe39076cbf6776346ad
joe <zjiuzhou@gmail.com>
2020-09-17 17:40:01 +0800

    add subset

---
+++ subsets/1.py
@@ -0,0 +1,18 @@
+def find_subsets(nums):
+    subsets = []
+    subsets.append([])
+    for num in nums:
+        for i in range(len(subsets)):
+            sub = list(subsets[i])
+            sub.append(num)
+            subsets.append(sub)
+
+    return subsets
+
+
+def main():
+    print("Here is the list of subsets: " + str(find_subsets([1, 3])))
+    print("Here is the list of subsets: " + str(find_subsets([1, 5, 3])))
+
+
+main()
